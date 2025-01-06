da2db1cf69fc3738aa8a8c1f7c439309b30cba1c
sunilmeena26 <sunilmeena641085@gmail.com>
2025-01-07 01:26:21 +0530

    Welcome Code In Java

---
+++ Welcome.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class{
+    public static void main(String []args){
+        System.out.println("Welcome To In The Calculator");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter A Number : ");
+        int a=sc.nextInt();
+        System.out.println("Enter A Number : ");
+        int a=sc.nextInt();
+        int c=a+b;
+        System.out.println("Sum Is : "+c);
+    }
+}
